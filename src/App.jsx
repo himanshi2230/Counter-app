b9146439eb48805638a9878b209abee5b34c6291
himanshi <itshimanshisingh@gmail.com>
2026-07-11 11:54:55 +0530

    frontend done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  
 
   return (
     <>
-      <h1>Counter App</h1>
+      <div className="container">
+        <h1>Counter App</h1>
+
+        <div className="count">0</div>
+
+        <div className="buttons">
+          <button>+</button>
+          <button>Reset</button>
+          <button>-</button>
+        </div>
+
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
